4405b82a00cc4f43b771e274d46f96ae2621de1c
imbrunosantoos <brunoribeirosouzasantos@gmail.com>
2026-05-30 11:29:32 +0100

    refactor(saveload): extrai guardarPilha do saveGame

--- proj/phase3/src/logic/saveload.c
+++ proj/phase3/src/logic/saveload.c
@@ -1,26 +1,26 @@
 #include "saveload.h"
 #include <stdio.h>
 
+/* escreve uma pilha no ficheiro: tamanho e depois cada carta (valor naipe) */
+static void guardarPilha(FILE *f, PilhaJogo *pilha) {
+    int j;
+    fprintf(f, "%d\n", pilha->cartas.size);
+    for (j = 0; j < pilha->cartas.size; j++)
+        fprintf(f, "%d %d\n", pilha->cartas.cards[j].value, pilha->cartas.cards[j].suit);
+}
+
 /* guarda o estado do jogo em ficheiro */
 void saveGame(char *nomePaciencia, PilhaJogo pilhas[], int numPilhas, char *ficheiro) {
-    int i, j;
+    int i;
     FILE *f = fopen(ficheiro, "w");
     if (f == NULL) {
         printf("Erro ao guardar jogo!\n");
         return;
     }
-
     fprintf(f, "%s\n", nomePaciencia);
     fprintf(f, "%d\n", numPilhas);
-
-    for (i = 0; i < numPilhas; i++) {
-        fprintf(f, "%d\n", pilhas[i].cartas.size);
-        for (j = 0; j < pilhas[i].cartas.size; j++) {
-            fprintf(f, "%d %d\n", pilhas[i].cartas.cards[j].value,
-                                   pilhas[i].cartas.cards[j].suit);
-        }
-    }
-
+    for (i = 0; i < numPilhas; i++)
+        guardarPilha(f, &pilhas[i]);
     fclose(f);
     printf("Jogo guardado!\n");
 }
